302b46902d05737718c08947950d860f881409d4
Ghislain Beaulac <SESA213338@se.com>
2019-07-26 15:42:39 -0400

    update example12 demo with only locales related

--- bootstrap3-demo-with-locales/src/app/examples/grid-localization.component.ts
+++ bootstrap3-demo-with-locales/src/app/examples/grid-localization.component.ts
@@ -7,14 +7,18 @@ import {
   FieldType,
   FileType,
   Filters,
+  Formatter,
   Formatters,
-  GridOption,
-  OperatorType
+  GridOption
 } from 'angular-slickgrid';
 import { localeFrench } from '../locales/fr';
 
-const NB_ITEMS = 500;
-const URL_SAMPLE_COLLECTION_DATA = 'assets/data/collection_500_numbers.json';
+const taskTranslateFormatter: Formatter = (row: number, cell: number, value: any, columnDef: any, dataContext: any, grid: any) => {
+  return value !== undefined ? `Titre ${value}` : '';
+};
+const exportBooleanFormatter: Formatter = (row: number, cell: number, value: any, columnDef: any, dataContext: any, grid: any) => {
+  return value ? 'Vrai' : 'Faux';
+};
 
 @Component({
   templateUrl: './grid-localization.component.html'
@@ -22,28 +26,7 @@ const URL_SAMPLE_COLLECTION_DATA = 'assets/data/collection_500_numbers.json';
 @Injectable()
 export class GridLocalizationComponent implements OnInit {
   title = 'Example 12: Localization with Locales - French Locale displayed';
-  subTitle = `Support external locales with the ngx-translate plugin, following these steps (<a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/Localization" target="_blank">Wiki docs</a>)
-  <ol class="small">
-    <li>You first need to "enableTranslate" in the Grid Options</li>
-    <li>In the Column Definitions, you have following options</li>
-    <ul>
-      <li>To translate a header title, use "translate key (i>
-      <li>For the cell values, you need to use a Formatter, there's 2 ways of doing it</li>
-      <ul>
-        <li>formatter: myCustomTranslateFormatter <b>&lt;= "Title" column uses it</b></li>
-        <li>formatter: Formatters.translate, i18n: this.translateService <b>&lt;= "Completed" column uses it</b></li>
-      </ul>
-    </ul>
-    <li>For date localization, you need to create your own custom formatter. </li>
-    <ul>
-      <li>You can easily implement logic to switch between Formatters "dateIso" or "dateUs", depending on current locale.</li>
-    </ul>
-    <li>For the Select (dropdown) filter, you can fill in the "label" property, if found it will use it, else it will use "label"</li>
-      <ul>
-        <li>What if your select options have totally different value/label pair? In this case, you can use the <b>customStructure: { label: 'customLabel', value: 'customValue'}</b> to change the property name(s) to use.'</li>
-        <li>What if you want to use "customStructure" and translation? Simply pass this flag <b>enableTranslateLabel: true</b></li>
-        <li>More info on the Select Filter <a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/Select-Filter" target="_blank">Wiki page</a>
-      </ul>
+  subTitle = `Support Custom Locales (through the "locales" grid option), following these steps (<a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/Localization" target="_blank">Wiki docs</a>)
     <li>For more info about "Download to File", read the <a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/Export-to-File" target="_blank">Wiki page</a></li>
     </ol>
   `;
@@ -59,20 +42,14 @@ export class GridLocalizationComponent implements OnInit {
 
   ngOnInit(): void {
     this.columnDefinitions = [
-      { id: 'title', name: 'Titre', field: 'id', sortable: true, minWidth: 100, filterable: true, params: { useFormatterOuputToFilter: true } },
+      { id: 'title', name: 'Titre', field: 'id', sortable: true, minWidth: 100, filterable: true, formatter: taskTranslateFormatter, params: { useFormatterOuputToFilter: true } },
       { id: 'description', name: 'Description', field: 'description', filterable: true, sortable: true, minWidth: 80 },
       {
         id: 'duration', name: 'Durée (jours)', field: 'duration', sortable: true,
+        formatter: Formatters.percentCompleteBar,
         minWidth: 100,
         filterable: true,
-        filter: {
-          collectionAsync: this.http.get<{ option: string; value: string; }[]>(URL_SAMPLE_COLLECTION_DATA),
-          customStructure: {
-            value: 'value',
-            label: 'label'
-          },
-          model: Filters.multipleSelect,
-        }
+        filter: { model: Filters.compoundSlider, operator: '>=' }
       },
       { id: 'start', name: 'Début', field: 'start', formatter: Formatters.dateIso, outputType: FieldType.dateIso, type: FieldType.date, minWidth: 100, filterable: true, filter: { model: Filters.compoundDate } },
       { id: 'finish', name: 'Fin', field: 'finish', formatter: Formatters.dateIso, outputType: FieldType.dateIso, type: FieldType.date, minWidth: 100, filterable: true, filter: { model: Filters.compoundDate } },
@@ -80,10 +57,11 @@ export class GridLocalizationComponent implements OnInit {
         id: 'completedBool', name: 'Complétée', field: 'completedBool', minWidth: 100,
         sortable: true,
         formatter: Formatters.checkmark,
+        exportCustomFormatter: exportBooleanFormatter,
         filterable: true,
         filter: {
-          collection: [{ value: '', label: '' }, { value: true, label: 'Vrai' }, { value: false, label: 'Faux' }],
-          model: Filters.singleSelect,
+          collection: [{ value: true, label: 'Vrai' }, { value: false, label: 'Faux' }],
+          model: Filters.multipleSelect,
           filterOptions: {
             autoDropWidth: true
           }
