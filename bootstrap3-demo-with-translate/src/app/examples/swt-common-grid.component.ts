9cfd6458627bc2b4b4cdc7739e1308ae8bf3f9d3
Ghislain Beaulac <SESA213338@se.com>
2019-08-26 19:17:32 -0400

    fix demo throwing some warnings with Angular 8

--- bootstrap3-demo-with-translate/src/app/examples/swt-common-grid.component.ts
+++ bootstrap3-demo-with-translate/src/app/examples/swt-common-grid.component.ts
@@ -56,7 +56,7 @@ export class SwtCommonGridComponent implements OnInit, AfterViewInit, BackendSer
     gridHeightString: string;
     gridWidthString: string;
 
-    @ViewChild('angularSlickGrid') angularSlickGrid: AngularSlickgridComponent;
+    @ViewChild('angularSlickGrid', { static: true }) angularSlickGrid: AngularSlickgridComponent;
 
     columnDefinitions: Column[] = [];
     dataset: any[];
