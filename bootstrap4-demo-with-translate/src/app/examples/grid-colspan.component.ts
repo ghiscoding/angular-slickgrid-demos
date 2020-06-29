f8b8cae847f51651256407002580348b75ae870b
Ghislain Beaulac <ghislain.beaulac@se.com>
2020-06-29 17:32:58 -0400

    update to latest version 2.19.0

--- bootstrap4-demo-with-translate/src/app/examples/grid-colspan.component.ts
+++ bootstrap4-demo-with-translate/src/app/examples/grid-colspan.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Column, FieldType, GridOption } from 'angular-slickgrid';
+import { AngularGridInstance, Column, FieldType, GridOption } from 'angular-slickgrid';
 
 @Component({
   templateUrl: './grid-colspan.component.html',
@@ -14,7 +14,7 @@ export class GridColspanComponent implements OnInit {
       Row Colspan - (<a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/Row-Colspan" target="_blank">Wiki docs</a>) /
       Header Grouping - (<a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/Header-Title-Grouping" target="_blank">Wiki docs</a>)
     </li>
-    <li>Note that you can add Sort but remember that it will sort by the data that the row contains, even if the data is visually hidden by colspan it will still sort it</li>
+    <li>Note that you can add Sort but remember that it will sort by the data which the row contains, even if the data is visually hidden by colspan it will still sort it</li>
     <li>
       Header Grouping spanning accross multiple columns is working but has some UI issues on window resize.
       If anyone can fix it, probably some CSS issues, please let us know.
@@ -22,6 +22,8 @@ export class GridColspanComponent implements OnInit {
   </ul>
   `;
 
+  angularGrid2: AngularGridInstance;
+  gridObj2: any;
   columnDefinitions1: Column[];
   columnDefinitions2: Column[];
   gridOptions1: GridOption;
@@ -32,6 +34,11 @@ export class GridColspanComponent implements OnInit {
   ngOnInit(): void {
     this.prepareGrid1();
     this.prepareGrid2();
+  }
+
+  angularGridReady2(angularGrid: AngularGridInstance) {
+    this.angularGrid2 = angularGrid;
+    this.gridObj2 = angularGrid.slickGrid;
   }
 
   prepareGrid1() {
@@ -51,9 +58,9 @@ export class GridColspanComponent implements OnInit {
       enableSorting: true,
       createPreHeaderPanel: true,
       showPreHeaderPanel: true,
-      preHeaderPanelHeight: 25,
+      preHeaderPanelHeight: 28,
       explicitInitialization: true,
-      colspanCallback: this.renderDifferentColspan
+      colspanCallback: this.renderDifferentColspan,
     };
 
     this.dataset1 = this.getData(500);
@@ -61,6 +68,7 @@ export class GridColspanComponent implements OnInit {
 
   prepareGrid2() {
     this.columnDefinitions2 = [
+      { id: 'sel', name: '#', field: 'num', behavior: 'select', cssClass: 'cell-selection', width: 40, resizable: false, selectable: false },
       { id: 'title', name: 'Title', field: 'title', sortable: true, columnGroup: 'Common Factor' },
       { id: 'duration', name: 'Duration', field: 'duration', columnGroup: 'Common Factor' },
       { id: 'start', name: 'Start', field: 'start', columnGroup: 'Period' },
@@ -77,7 +85,9 @@ export class GridColspanComponent implements OnInit {
       showPreHeaderPanel: true,
       preHeaderPanelHeight: 25,
       explicitInitialization: true,
-      frozenColumn: 1,
+      frozenColumn: 2,
+      gridMenu: { hideClearFrozenColumnsCommand: false },
+      headerMenu: { hideFreezeColumnsCommand: false }
     };
 
     this.dataset2 = this.getData(500);
@@ -99,6 +109,11 @@ export class GridColspanComponent implements OnInit {
       };
     }
     return mockDataset;
+  }
+
+  setFrozenColumns2(frozenCols: number) {
+    this.gridObj2.setOptions({ frozenColumn: frozenCols, alwaysShowVerticalScroll: false });
+    this.gridOptions2 = this.gridObj2.getOptions();
   }
 
   /**
