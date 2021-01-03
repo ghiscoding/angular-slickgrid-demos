01aed99927f8f8cc7d2758e306ed0f6688f3f131
Jared Scott <jared.scott@variable.team>
2021-01-03 11:43:10 +0800

    Add Demonstration for Bootstrap 5. In conjunction with some minor additions to Angular-Slickgrid Bootstrap theme.

---
+++ bootstrap5-demo-with-translate/README.md
@@ -0,0 +1,15 @@
+## Bootstrap 4 
+Common Bootstrap 4 demo using `ngx-translate` that allows for using multiple languages (locales) and switch them on the fly.
+
+### Install and Start Demo
+```bash
+git clone https://github.com/ghiscoding/angular-slickgrid-demos
+cd bootstrap4-demo
+npm install # OR yarn install
+npm start # OR yarn start
+```
+
+### Build Demo
+```bash
+npm run build # OR yarn run build
+```
---
+++ bootstrap5-demo-with-translate/src/app/examples/grid-basic.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { Column, GridOption, Formatters } from 'angular-slickgrid';
+
+const NB_ITEMS = 995;
+
+@Component({
+  templateUrl: './grid-basic.component.html'
+})
+export class GridBasicComponent implements OnInit {
+  title = 'Example 1: Basic Grids';
+  subTitle = `
+    Basic Grids with fixed sizes (800 x 225) set by "gridHeight" &amp; "gridWidth"
+    <ul>
+      <li><a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/HOWTO---Step-by-Step" target="_blank">Wiki HOWTO link</a></li>
+    </ul>
+  `;
+
+  columnDefinitions1: Column[];
+  columnDefinitions2: Column[];
+  gridOptions1: GridOption;
+  gridOptions2: GridOption;
+  dataset1: any[];
+  dataset2: any[];
+
+  ngOnInit(): void {
+    this.columnDefinitions1 = [
+      { id: 'title', name: 'Title', field: 'title', sortable: true },
+      { id: 'duration', name: 'Duration (days)', field: 'duration', sortable: true },
+      { id: '%', name: '% Complete', field: 'percentComplete', sortable: true },
+      { id: 'start', name: 'Start', field: 'start', formatter: Formatters.dateIso },
+      { id: 'finish', name: 'Finish', field: 'finish', formatter: Formatters.dateIso },
+      { id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven', sortable: true }
+    ];
+    this.gridOptions1 = {
+      enableAutoResize: false,
+      enableSorting: true,
+    };
+
+    // copy the same Grid Options and Column Definitions to 2nd grid
+    // but also add Pagination in this grid
+    this.columnDefinitions2 = this.columnDefinitions1;
+    this.gridOptions2 = {
+      ...this.gridOptions1,
+      ...{
+        enablePagination: true,
+        pagination: {
+          pageSizes: [5, 10, 20, 25, 50],
+          pageSize: 5
+        },
+      }
+    };
+
+    // mock some data (different in each dataset)
+    this.dataset1 = this.mockData(NB_ITEMS);
+    this.dataset2 = this.mockData(NB_ITEMS);
+  }
+
+  mockData(count: number) {
+    // mock a dataset
+    const mockDataset = [];
+    for (let i = 0; i < count; i++) {
+      const randomYear = 2000 + Math.floor(Math.random() * 10);
+      const randomMonth = Math.floor(Math.random() * 11);
+      const randomDay = Math.floor((Math.random() * 29));
+      const randomPercent = Math.round(Math.random() * 100);
+
+      mockDataset[i] = {
+        id: i,
+        title: 'Task ' + i,
+        duration: Math.round(Math.random() * 100) + '',
+        percentComplete: randomPercent,
+        start: new Date(randomYear, randomMonth + 1, randomDay),
+        finish: new Date(randomYear + 1, randomMonth + 1, randomDay),
+        effortDriven: (i % 5 === 0)
+      };
+    }
+
+    return mockDataset;
+  }
+}
