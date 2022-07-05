21e78597dfce34aa6a4b195fb50e28c924998a26
ghiscoding <gbeaulac@gmail.com>
2022-07-05 17:56:55 -0400

    chore: update to latest Angular-Slickgrid version 4.2.2

--- bootstrap4-demo-with-translate/src/app/app-routing.module.ts
+++ bootstrap4-demo-with-translate/src/app/app-routing.module.ts
@@ -75,7 +75,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'corrected' })],
   exports: [RouterModule],
 })
 export class AppRoutingRoutingModule { }
